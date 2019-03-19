a100e956f687f400a2b93432b89c293fece19108
Martijn Schoemaker <mschoemaker@wehkamp.nl>
2019-03-19 20:03:35 +0100

    Initial commit

---
+++ ModularizeSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModularizeSampleApp
+//
+//  Created by Martijn Schoemaker on 19/03/2019.
+//  Copyright © 2019 martijnschoemaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,20 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '10.0'
+use_frameworks!
+
+workspace 'ModularizeSampleApp'
+target 'ModularizeSampleApp' do
+	project 'ModularizeSampleApp'
+
+  pod 'Swinject'
+	pod 'SwinjectStoryboard'
+	pod 'RxSwift'
+	pod 'RxRetroSwift' 
+
+end
+
+target 'ModularizeSampleAppUITests' do
+	project 'ModularizeSampleApp'
+
+end
